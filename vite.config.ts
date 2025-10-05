0eba8a9d090e8494bc800d76c546a5b832bde84d
Abdul Majeed <a.majeed2jz@gmail.com>
2025-10-05 16:41:45 +0500

    Fix: ensure dist folder exists before deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/notes-app-with-cicd/'
-})
+  base: "/notes-app-with-cicd/",
+  build: {
+    outDir: "dist",
+  },
+});
